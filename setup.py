5cb1d9dfaa3ea56173c435651dcb2f14c00feed2
Guillaume Vincent <gvincent@oslab.fr>
2015-02-16 11:05:14 +0100

    fix pypi error

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(
     version=__version__,
     author='Guillaume Vincent',
     author_email='gvincent@oslab.fr',
-    packages=['smartconfigparser', 'smartconfigparser.tests', 'README.md'],
+    packages=['smartconfigparser', 'smartconfigparser.tests'],
     url='http://gitlab.oslab.fr/oslab/smartconfigparser.git',
     license='wtfpl - Do what the fuck you want to public license',
     description='SmartConfigParser add missing default value in ConfigParser module',
-    long_description=open('README.md').read(),
     install_requires=[],
 )
