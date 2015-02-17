0722096c697b1ac2c2a0610501680d3107637d5a
Guillaume Vincent <gvincent@oslab.fr>
2015-02-17 22:08:32 +0100

    add 0.0.4 to pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup(
     name='smartconfigparser',
-    version='0.0.3',
+    version='0.0.4',
     author='Guillaume Vincent',
     author_email='gvincent@oslab.fr',
     py_modules=['smartconfigparser'],
-    url='http://gitlab.oslab.fr/oslab/smartconfigparser.git',
+    url='https://github.com/guillaumevincent/smartconfigparser.git',
     license='wtfpl - Do what the fuck you want to public license',
-    description='SmartConfigParser add missing default value in ConfigParser module',
-    install_requires=[],
+    description='SmartConfigParser add missing default value in ConfigParser module'
 )
